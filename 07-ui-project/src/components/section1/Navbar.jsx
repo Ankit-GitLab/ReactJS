d38c312969c63a514dcf65816dded640b535b29e
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-17 23:47:30 +0530

    07-ui-project

---
+++ 07-ui-project/src/components/section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-8 py-18'>
+        <h4 className='bg-black text-white uppercase px-6 py-2 rounded-full'>Target Audience</h4>
+        <button className='bg-gray-100 px-6 py-2 uppercase rounded-full tracking-widest text-sm'>digital Banking Plateform</button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 07-ui-project/src/components/section1/RightContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RightContent = () => {
+  return (
+    <div className='h-screen w-3/4 bg-blue-600'>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default RightContent
