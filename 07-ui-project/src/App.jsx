f02de22508bfceb2f2330237499751f4131fb9f0
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-18 23:33:15 +0530

    07-ui-project

--- 07-ui-project/src/App.jsx
+++ 07-ui-project/src/App.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 import Section1 from './components/section1/Section1'
 import Section2 from './components/section2/Section2'
 
+const users = [
+  {
+    color: '#8B5CF6',
+    img: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330',
+    tag: 'Developer'
+  },
+  {
+    color: '#EC4899',
+    img: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e',
+    tag: 'Designer'
+  },
+  {
+    color: '#3B82F6',
+    img: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d',
+    tag: 'Engineer'
+  }
+]
+
 const App = () => {
   return (
     <div>
-      <Section1 />
+      <Section1 users={users} />
       <Section2 />
     </div>
   )
--- 07-ui-project/src/components/section1/RightContent.jsx
+++ 07-ui-project/src/components/section1/RightContent.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+const RightContent = (props) => {
+  console.log(props.users);
 
-const RightContent = () => {
   return (
-    <div className='h-screen w-3/4 bg-blue-600'>
-        <div></div>
-        <div></div>
+    <div id="right" className="h-full flex rounded-4xl overflow-x-auto flex-nowrap gap-10 p-6 w-2/3">
+      {props.users.map(function (elem, idx) {
+        return (
+          <RightCard
+            key={idx}
+            color={elem.color}
+            id={idx}
+            img={elem.img}
+            tag={elem.tag}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default RightContent
+export default RightContent;
