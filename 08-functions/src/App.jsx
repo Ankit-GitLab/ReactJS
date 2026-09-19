4eb050d9128a83d50061620754e6ac317e6a2a6e
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-19 14:32:07 +0530

    completed the function

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
-
+import React from 'react';
 
 const App = () => {
 
+  const pageScrolling = (elem) => {
+    if (elem > 0) {
+      console.log('seedha scrolling........');
+    } else {
+      console.log('ulta scrolling.........');
+    }
+  };
 
   return (
     <div>
-      <button onClick={function(){
-        console.log('hello guys');
-        
-      }}>Click here</button>
+      <div>
+        <button onClick={function () {
+          console.log('hello guys');
+        }}>
+          Click here
+        </button>
 
-      <input onChange={function(elem){
-        console.log(elem.target.value);
-        
-      }}type="text" placeholder='Enter name'/>
+        <input
+          onChange={function (elem) {
+            console.log(elem.target.value);
+          }}
+          type="text"
+          placeholder="Enter name"
+        />
 
-      <div onMouseMove={(elem)=>{
-          console.log(elem.clientX);
-          
-      }}className='box'></div>
+        <div
+          onMouseMove={(elem) => {
+            console.log(elem.clientX);
+          }}
+          className="box"
+        ></div>
+      </div>
 
+      <div
+        onWheel={(elem) => {
+          pageScrolling(elem.deltaY);
+        }}
+      >
+        <div className="page1"></div>
+        <div className="page2"></div>
+        <div className="page3"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
