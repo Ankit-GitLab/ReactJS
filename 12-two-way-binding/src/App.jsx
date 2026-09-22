46f746e81a8ba5f772a739cbaf11864efb87bd25
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-23 00:03:13 +0530

    12-two-way-binding

--- 12-two-way-binding/src/App.jsx
+++ 12-two-way-binding/src/App.jsx
@@ -1,43 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
+
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const [task, setTask] = useState([])
+
   const submitHandler = (e)=>{
     e.preventDefault()
-    console.log('form submitted');
+
+    const copyTask = [...task];
+
+    copyTask.push({title, details})  
+
+    setTask(copyTask);
+
+    console.log(task);
+    
+     
+
+    setTitle('')
+    setDetails('')
     
   }
 
+
   return (
     <div className='h-screen lg:flex bg-black text-white '>
+
       <form onSubmit={(e)=>{
         submitHandler(e)
       }}className='flex gap-4 lg:w-1/2 items-start flex-col p-10'>
+
+          <h1 className='text-4xl font-bold'>Add Notes</h1>
+
+          {/* phela input for heading */}
           <input 
             type="text" 
             placeholder='Enter Notes Heading'
-            className='px-5 w-full py-2 font-medium border-2 outline-none rounded' 
+            className='px-5 w-full py-2 font-medium border-2 outline-none rounded'
+            value={title}
+            onChange={(e)=>{
+              setTitle(e.target.value)
+            }} 
             />
 
+            {/* detailed vala input  */}
             <textarea 
-            type="text"
-            className='px-5 w-full h-32 py-2 font-medium flex items-start flex-row border-2 outline-none rounded'
-            placeholder='Write Details'
+              type="text"
+              className='px-5 w-full h-32 py-2 font-medium flex items-start flex-row border-2 outline-none rounded'
+              placeholder='Write Details Here'
+              value={details}
+              onChange={(e) => {
+                setDetails(e.target.value)
+              }}
             />
 
-            <button className='bg-white font-medium w-full text-black outline-none px-5 py-2 rounded' >Add Notes</button>
+            <button className='bg-white active:scale-95 font-medium w-full text-black outline-none px-5 py-2 rounded'
+            >
+              Add Notes
+            </button>
+
       </form>
-      <div className='lg:w-1/2 bg-gray-900 p-10'>
-      <h1 className='text-3xl font-bold'>Your Notes</h1>
+      <div className='lg:w-1/2 lg:border-l p-10'>
+      <h1 className='text-4xl font-bold'>Your Notes</h1>
       <div className='flex flex-wrap gap-5 m-5 h-full overflow-auto'>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        {task.map(function(elem, idx){
+
+          return <div key={idx} className='h-52 w-40 rounded-xl text-black p-4 bg-white'>
+            <h3 className='leading-tight text-xl font-bold'>{elem.title}</h3>
+            
+            <p>{elem.details}</p>
+          </div>
+
+        })}
       </div>
       </div>
     </div>
