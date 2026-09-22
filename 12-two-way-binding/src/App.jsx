9239ef651cba5c892e15545abee74d9a7d407678
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-22 10:31:43 +0530

    12-two-way-binding

--- 12-two-way-binding/src/App.jsx
+++ 12-two-way-binding/src/App.jsx
@@ -1,26 +1,45 @@
 import React from 'react'
 
 const App = () => {
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log('form submitted');
+    
+  }
+
   return (
-    <div className='h-screen bg-black text-white'>
-      <form className='flex items-start flex-col p-10'>
-        <div className='flex gap-4  w-1/2 items-startn flex-col'>
+    <div className='h-screen lg:flex bg-black text-white '>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}className='flex gap-4 lg:w-1/2 items-start flex-col p-10'>
           <input 
             type="text" 
             placeholder='Enter Notes Heading'
-            className='px-5 w-full py-2 border-2 outline-none rounded' 
+            className='px-5 w-full py-2 font-medium border-2 outline-none rounded' 
             />
 
-            <input 
+            <textarea 
             type="text"
-            className='px-5 w-full h-20 py-2 border-2 outline-none rounded'
+            className='px-5 w-full h-32 py-2 font-medium flex items-start flex-row border-2 outline-none rounded'
             placeholder='Write Details'
             />
 
-            <button className='bg-white w-full text-black outline-none px-5 py-2 rounded' >Add Notes</button>
-        </div>
-        <img className='h-50'  src="https://static.vecteezy.com/system/resources/thumbnails/049/578/155/small/a-black-and-white-drawing-of-a-man-writing-png.png"alt="" />
+            <button className='bg-white font-medium w-full text-black outline-none px-5 py-2 rounded' >Add Notes</button>
       </form>
+      <div className='lg:w-1/2 bg-gray-900 p-10'>
+      <h1 className='text-3xl font-bold'>Your Notes</h1>
+      <div className='flex flex-wrap gap-5 m-5 h-full overflow-auto'>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+      </div>
+      </div>
     </div>
   )
 }
