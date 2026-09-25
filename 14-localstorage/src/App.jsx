a55a95e3b9cad85e9faf15a3ed103bf491419192
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-25 15:54:10 +0530

    14-localstorage completed

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,8 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [age, setAge] = useState('')
+  const [city, setCity] = useState('')
+
+  const [user, setUser] = useState(null)
+
+  // SET ITEM
+  const saveUser = () => {
+
+    const userData = {
+      username: username,
+      age: age,
+      city: city
+    }
+
+    localStorage.setItem('user', JSON.stringify(userData))
+
+    setUser(userData)
+
+    console.log('User saved:', userData)
+  }
+
+  // GET ITEM
+  const getUser = () => {
+
+    const data = localStorage.getItem('user')
+
+    if (data) {
+      const userData = JSON.parse(data)
+
+      setUser(userData)
+
+      console.log('User:', userData)
+    } else {
+      alert('No user found!')
+    }
+  }
+
+  // REMOVE ITEM
+  const removeUser = () => {
+
+    localStorage.removeItem('user')
+
+    setUser(null)
+
+    console.log('User removed')
+  }
+
   return (
-    <div>App</div>
+    <div>
+
+      <h1>LocalStorage User Manager</h1>
+
+      <input
+        type="text"
+        placeholder="Enter username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="number"
+        placeholder="Enter age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <br /><br />
+
+      <div className='button'>
+        <button onClick={saveUser}>
+        Save User
+      </button>
+
+      <button onClick={getUser}>
+        Get User
+      </button>
+
+      <button onClick={removeUser}>
+        Remove User
+      </button>
+      </div>
+
+      <hr />
+
+      {user && (
+        <div>
+          <h2>User Details</h2>
+
+          <p>Username: {user.username}</p>
+          <p>Age: {user.age}</p>
+          <p>City: {user.city}</p>
+        </div>
+      )}
+
+    </div>
   )
 }
 
