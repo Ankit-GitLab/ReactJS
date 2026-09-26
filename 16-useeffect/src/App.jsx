49be23a4e4b487b5fc6774b6afa963767a64833c
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-26 10:52:03 +0530

    16-useEffect completed

--- 16-useeffect/src/App.jsx
+++ 16-useeffect/src/App.jsx
@@ -1,28 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState(0)
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
 
-  const [num2, setNum2] = useState(100)
+  function aChanging(){
+    console.log('A ki value change hogi');
+    
+  }
+
+  function bChanging(){
+    console.log('A ki value change hogi');
+  }
 
   useEffect(function(){
-    console.log('use effect is running..........');
-    
-  }, [num])
+    aChanging();
+    console.log('use effect is running.......'); 
+  },[a])
+
+  useEffect(function(){
+    bChanging();
+    console.log('use effect is running.......'); 
+  },[b])
 
   return (
     <div>
-      <h1>num {num}</h1>
-      <h1>num2 {num2}</h1>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+
       <button 
-      onMouseEnter={()=>{
-        setNum(num+1);
+      onClick={()=>{
+        setA(a+1)
       }}
-      onMouseLeave={()=>{
-        setNum2(num2+1)
+      >Change A</button>
+
+      <button
+      onClick={()=>{
+        setB(b-1)
       }}
-      >Hower</button>
+      >Change B</button>
     </div>
   )
 }
